29aff978a259a57e5e25db800cc124336bbcebe9
mfahriferdiansyah <xkadzuvic@gmail.com>
2023-05-10 22:28:51 +0700

    feat: add footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,10 @@
+<script></script>
+
+<template>
+  <footer class="container-fluid text-center text-lg-start bg-light text-muted ">
+    <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+      © 2023 Copyright:
+      <a class="text-reset fw-bold" href="/">VueCRUD</a>
+    </div>
+  </footer>
+</template>
